13c967ab7bf07eb64acd8fa21796cf86c2e5ffaf
mmginc <ivannedoklanov@gmail.com>
2010-11-25 21:54:44 +0000

    new class to represent obstacles on the lanes

---
+++ Obstacles.java
@@ -0,0 +1,16 @@
+import java.awt.geom.Rectangle2D;
+
+
+public class Obstacles extends Rectangle2D.Double
+{
+
+	public Obstacles(double x, double y) 
+	{
+		super(x, y, 30, 30);
+	}
+	
+	public void moveDown() 
+	{
+		setRect(this.getX(), this.getY()+1, 30, 30);
+	}
+}
