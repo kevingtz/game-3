a99a22d2a34dfb054f1d318a32c36e6027ef710f
nikola <nnedoklanov@gmail.com>
2010-11-25 21:37:11 +0000

    almost a menu

--- GameFrame.java
+++ GameFrame.java
@@ -6,21 +6,43 @@ import javax.swing.JFrame;
  */
 public class GameFrame extends JFrame 
 {
+	private ButtonDemo menu;
+	
 	public GameFrame() {
 	    setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 	    setSize(500, 500);
+	    menu = new ButtonDemo(this);
+	    this.shwoMenu();
+//	    this.runGame();
+	    
+	}
 	
-	    Car car = new Car();
+	public void shwoMenu()
+	{	
+        //Create and set up the window.
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Create and set up the content pane.
+        menu.setOpaque(true); //content panes must be opaque
+        this.setContentPane(menu);
+        //Display the window.
+        this.setVisible(true);
+	}
+	
+    void runGame()
+    {
+    	System.out.println("SAAAAAAAAAAAA");
+    	Car car = new Car();
 	    Car[] cars = new Car[0];
-//	    Lane[] lanes = new Lane[0];
+	    
 	    DrawComponent dc = new DrawComponent(3, car, cars, getHeight());
-	    add(dc);
-	
+	    
+	    this.setContentPane(dc);
+	    this.setVisible(true);
 	    MoveCars mc = new MoveCars(car,getHeight(),dc);
 	    
 	    MoveListener kl = new MoveListener(car);
 	    addKeyListener(kl);
 
 	    mc.start();
-	}
+    }
 }
--- Main.java
+++ Main.java
@@ -9,6 +9,14 @@ public class Main
     public static void main(String[] args) 
     {
     	GameFrame frame = new GameFrame();
+    	
+    	javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+//                ButtonDemo b = new ButtonDemo();
+//                ButtonDemo.createAndShowGUI(); 
+            }
+        });
+    	
     	frame.setVisible(true);
     }
 }
